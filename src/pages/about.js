cbb1a44b6eb199269e800a53c7d2b7951b664a36
hamzamughal <mughalh473@gmail.com>
2021-04-07 11:05:08 +0500

    set static image for about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import Layout from '../components/Layout'
-
+import { StaticImage } from "gatsby-plugin-image"
+import { Link } from "gatsby"
 
 const About = () => {
     return (
         <Layout>
-            <h2>Hello from About Page</h2>
+            <main className="page">
+                <section className="about-page">
+                    <article>
+                        <h2>Prism edison bulb vaporware normcore</h2>
+                        <p>Pickled tbh farm-to-table keytar freegan, ennui hell of coloring book food truck cornhole echo park</p>
+                        <p>Semiotics iPhone craft beer mlkshk meditation pinterest bespoke</p>
+                        <Link to="/contact" className="btn">contact</Link>
+                    </article>
+                    <StaticImage src="../assets/images/about.jpeg" alt="aboutimg" className="about-img" placeholder="blurred" />
+                </section>
+            </main>
         </Layout>
     )
 }
