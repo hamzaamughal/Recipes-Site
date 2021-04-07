14e29cdd938d306cf6276d189ee2b1fb7c4eef33
hamzamughal <mughalh473@gmail.com>
2021-04-07 14:12:20 +0500

    set static image for home page

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,10 +6,34 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: 'Simply Recipes',
+    description: 'Nice and clean recipes site',
+    author: '@hamza_mughal',
+    person: {
+      name: "ali",
+      age: 32
+    },
+    simpleData: ['item 1', 'item 2'],
+    complexData: [{
+      name: "abubakar",
+      age: 30
+    }, {
+      name: "susan",
+      age: 22
+    }]
+  },
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
   ],
 }
