c22a506a43b8935a0e9ca1d12b6a5cee9b424c12
hamzamughal <mughalh473@gmail.com>
2021-04-13 16:53:38 +0500

    complete dynamic pages using gatsby-node method

--- src/templates/tag-template.js
+++ src/templates/tag-template.js
@@ -1,11 +1,38 @@
 import React from 'react'
+import { graphql } from 'gatsby'
+import RecipesList from '../components/RecipesList'
+import Layout from '../components/Layout'
 
-const TagTemplate = () => {
+const TagTemplate = ({ data, pageContext }) => {
+    const recipes = data.allContentfulRecipe.nodes
     return (
-        <div>
-            <h3>Tag template page</h3>
-        </div>
+        <Layout>
+            <main className="page">
+                <h2>{pageContext.tag}</h2>
+                <div className="tag-recipes">
+                    <RecipesList recipes={recipes} />
+                </div>
+            </main>
+        </Layout>
     )
 }
 
+export const query = graphql`
+query GetRecipeByTag($tag: String) {
+    allContentfulRecipe(
+        sort: { fields: title, order: ASC }
+      filter: { content: { tags: { eq: $tag } } }
+    ) {
+        nodes {
+            title
+            id
+            prepTime
+            cookTime
+            image {
+                gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED)
+            }
+        }
+    }
+}
+`
 export default TagTemplate
