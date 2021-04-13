9cc65129dae02b8c6efd98ce34f9a51288517ead
hamzamughal <mughalh473@gmail.com>
2021-04-13 16:21:13 +0500

    dynamic pages using gatsby-node method

---
+++ src/templates/tag-template.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TagTemplate = () => {
+    return (
+        <div>
+            <h3>Tag template page</h3>
+        </div>
+    )
+}
+
+export default TagTemplate
